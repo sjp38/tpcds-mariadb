19e3dbde9e195cd1c069f358d306620d9f391817
SeongJae Park <sj38.park@gmail.com>
2017-09-25 14:42:17 +0900

    Add TPC-DS data generation script
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ datgen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <scale factor>"
+	exit 1
+fi
+
+SF=$1
+DATA_DIR=data_`printf %02d $SF`
+
+pushd tpcds-kit/tools
+
+if [ -d $DATA_DIR ]
+then
+	echo "Data exists.  Skip."
+	exit 0
+fi
+
+mkdir $DATA_DIR
+./dsdgen -scale $SF -dir $DATA_DIR
