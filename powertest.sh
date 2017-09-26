234e22fca6d3ef0d735492d63048151ee96db8a6
SeongJae Park <sj38.park@gmail.com>
2017-09-26 13:29:31 +0900

    Add powertest script
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ powertest.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Do Power Test of TPC-DS
+
+BINDIR=`dirname $0`
+
+USER=root
+
+# Usage: $0 [user [password] ]
+
+if [ $# -eq 2 ]
+then
+	USER=$1
+	PASSWORD=$2
+fi
+
+function msecs() {
+	echo $((`date +%s%N` / 1000000))
+}
+
+function msec_to_sec() {
+	MSECS=$1
+	SECS=$(($MSECS / 1000))
+	MSECS=$(($MSECS - $SECS * 1000))
+	printf %d.%03d $SECS $MSECS
+}
+
+MYSQL="/usr/local/mysql/bin/mysql -u $USER"
+if [ ! -z $PASSWORD ]
+then
+	MYSQL="$MYSQL -p $PASSWORD"
+fi
+
+MYSQL="$MYSQL tpcds"
+
+TOTAL_MSECONDS=0
+# We use only 50 queries
+for q in {1..50}
+do
+	START=`msecs`
+	./runquery.sh $q > /dev/null
+	END=`msecs`
+	DURATION=$(( $END - $START))
+	printf "%d: \t%16s secs\n" $q `msec_to_sec $DURATION`
+	TOTAL_MSECONDS=$(($TOTAL_MSECONDS + $DURATION))
+done
+
+printf "Total: \t%16s secs\n" `msec_to_sec $TOTAL_MSECONDS`
