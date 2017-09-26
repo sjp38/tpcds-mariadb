3868c21259e9ef8d74f9f3d3831b1828e8a2e86a
SeongJae Park <sj38.park@gmail.com>
2017-09-26 11:54:50 +0900

    Add patch for tpcds-kit apply script
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ patch-for-ubuntu16.04.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BINDIR=`dirname $0`
+
+pushd $BINDIR
+
+TPCDSKIT=./tpcds-kit
+
+if [ ! -d $TPCDSKIT ]
+then
+	echo "tpcds kit source code not found under $TPCDSKIT/."
+	exit 1
+fi
+
+pushd $TPCDSKIT
+patch -p1 < ../for-ubuntu16.04.patch
+popd
+
+popd
